0a9ea26a2776cfc71030b5b13f00a0d699598b6c
Joseph-K-B <303josephb@gmail.com>
2021-06-02 22:10:31 -0600

    app.js

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-console.log("Hello!");
-const myElement= document.getElementById('top_section');
+/* eslint-disable no-console */
+console.log('Hello!');
+const myElement = document.getElementById('top_section');
 console.log(myElement);
+
+
 // HTML setup
 // grab DOM elements (input and button)
 // get access to user's input
 // use the input to "do something"
 // Display the result to the user
-const  submitbtn= document.getElementById('submit-btn');
-const  inputField= document.getElementById('input-Field');
-const middleSection= document.getElementById('middle-section');
-const topSection= document.getElementById('top-section');
-const colorField= document.getElementById('color-Field');
-const pronounField= document.getElementById('pronoun-Field');
-const submitprobtn= document.getElementById('pronoun-Field');
+
+//Name color & pronoun constants
+const submitbtn = document.getElementById('submit-btn');
+const inputField = document.getElementById('input-Field');
+const middleSection = document.getElementById('middle-section');
+const topSection = document.getElementById('top-section');
+const colorField = document.getElementById('color-Field');
+const pronounField = document.getElementById('submitPro-btn');
+const submitprobtn = document.getElementById('submitPro-btn');
+
+//Name and color btns
 submitbtn.addEventListener('click', ()=>{
     
-    console.log(inputField.value);
-    console.log(middleSection.textContent);
-    middleSection.textContent=inputField.value;
-    console.log(colorField.value);
-    topSection.style.backgroundColor = colorField.value;
     
-    }
-)
+    console.log(middleSection.textContent);
+    middleSection.textContent = inputField.value;
+    console.log(inputField.value);
+    console.log(colorField.value);
+    topSection.style.backgroundColor = colorField.value;}
+);
+
+//pronoun btns
 submitprobtn.addEventListener('click', () =>{
-    console.log(pronoun-Field.value);
-    bottomSection.textContent = pronoun-Field.value;
-})
+
+
+    console.log(pronounField.textContent);
+    pronounField.textContent = pronounField.value;} 
+);
